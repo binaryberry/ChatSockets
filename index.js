b57b7daae91b601a62cddb8cb38a559a8530a6c6
babelberry <tatienantes@yahoo.fr>
2014-09-23 19:47:28 +0100

    displays messages to the user when someone enters or leaves the room

--- index.js
+++ index.js
@@ -6,6 +6,11 @@ app.get('/', function(req, res){
   res.sendfile('index.html');
 });
 
+io.sockets.on('connection', function (socket) {
+	socket.broadcast.emit('user connected');
+});
+
+
 io.on('connection', function(socket){
   socket.on('chat message', function(msg){
     io.emit('chat message', msg);
